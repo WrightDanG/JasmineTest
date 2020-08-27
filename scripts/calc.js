99e3c2a396ad853678ac4e8999307424080b205a
WrightDanG <daniel-g-wright@hotmail.co.uk>
2020-08-27 13:33:50 +0000

    Amended the new calculator to allow for new instances for each test.

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,8 +1,12 @@
-function addition(numOne, numTwo) {
-    if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
-        return numOne + numTwo;
+Calculator = function() {
+    this.value = 0;
+}
+
+//This represents a full functionality change to our original calculator model.
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
+        this.value += number;
     } else {
         alert("Error!");
     }
-    
 }
